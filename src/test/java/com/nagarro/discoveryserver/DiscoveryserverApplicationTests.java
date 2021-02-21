a8677b1ec78ec594b46321d2cef78d038d4d4e17
Vatsal Hirpara <vatsalplay9@gmail.com>
2021-02-21 16:40:34 +0530

    initial commit

---
+++ src/test/java/com/nagarro/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
